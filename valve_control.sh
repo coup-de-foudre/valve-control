bcc225fef879e2d4179426ff671f8e6476d6ce0b
Mike McCoy <michael.b.mccoy@gmail.com>
2019-08-17 11:54:21 -0700

    Added deploy scripts

--- valve_control.sh
+++ valve_control.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-PYTHONAPATH=${DIR}:${PYTHONPATH} python3 -m valve_control.relay_controller
+PYTHONPATH=${DIR}:${PYTHONPATH} python3 -m valve_control.relay_controller
